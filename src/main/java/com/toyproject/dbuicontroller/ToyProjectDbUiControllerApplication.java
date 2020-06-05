2cf7a1c21eef80b3dcae051709c4b3ae1df6d766
jakyoungkim <jakyoung.kim@catenoid.net>
2020-06-05 10:31:02 +0900

    toyproject init config

---
+++ src/main/java/com/toyproject/dbuicontroller/ToyProjectDbUiControllerApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.dbuicontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectDbUiControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyProjectDbUiControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyproject/dbuicontroller/ToyProjectDbUiControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.dbuicontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectDbUiControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
